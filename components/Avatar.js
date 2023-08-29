e9c0e30fe527cd3fd081001fff50fe75f231fea2
couba45 <103531034+couba45@users.noreply.github.com>
2023-08-28 21:53:00 -0500

    raw image

--- components/Avatar.js
+++ components/Avatar.js
@@ -6,7 +6,7 @@ const Avatar = () => {
     <div className="hidden xl:flex xl:max-w-none">
       <Image
         src={
-          "https://github.com/couba45/diego-portfolio/blob/28a8bb929a343d32931c002254feb4b55953509a/public/avatar-no-bg.png"
+          "https://raw.githubusercontent.com/couba45/diego-portfolio/main/public/avatar-no-bg.png"
         }
         width={737}
         height={678}
