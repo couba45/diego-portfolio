9c43a1133689f7fa5581440bbe072c5d1b9db90c
couba45 <103531034+couba45@users.noreply.github.com>
2023-08-28 21:48:50 -0500

    image added

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,9 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
       <Image
-        src={"/avatar-no-bg.png"}
+        src={
+          "https://github.com/couba45/diego-portfolio/blob/28a8bb929a343d32931c002254feb4b55953509a/public/avatar-no-bg.png"
+        }
         width={737}
         height={678}
         alt=""
