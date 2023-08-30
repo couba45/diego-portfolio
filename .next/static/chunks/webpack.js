6308b9ca28fa7f8503085301373910a558d822af
couba45 <103531034+couba45@users.noreply.github.com>
2023-08-30 00:50:28 -0500

    link used with anchor tag

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a8a4c3c2aa4075e5"; }
+/******/ 		__webpack_require__.h = function() { return "05ac0344f2617446"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
