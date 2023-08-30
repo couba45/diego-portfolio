8d8cef72bfbb113aaa68d88bcfb03402817de7c7
couba45 <103531034+couba45@users.noreply.github.com>
2023-08-29 23:14:50 -0500

    slider deleted for convinient

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -44,40 +44,45 @@ const serviceData = [
 
 const ServiceSlider = () => {
   return (
-    <Swiper
-      breakpoints={{
-        320: { slidesPerView: 1, spaceBetween: 15 },
-        640: { slidesPerView: 3, spaceBetween: 15 },
-      }}
-      freeMode={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[FreeMode, Pagination]}
-      className="h-[240px]  sm:h-[340px] "
-    >
+    // <Swiper
+    //   breakpoints={{
+    //     320: { slidesPerView: 1, spaceBetween: 15 },
+    //     640: { slidesPerView: 3, spaceBetween: 15 },
+    //   }}
+    //   freeMode={true}
+    //   pagination={{
+    //     clickable: true,
+    //   }}
+    //   modules={[FreeMode, Pagination]}
+    //   className="h-[240px]  sm:h-[340px] "
+    // >
+    <div className="h-[240px] sm:h-[340px] flex flex-col xl:flex-row items-center">
       {serviceData.map((item, itemIndex) => {
         return (
-          <SwiperSlide key={itemIndex}>
-            <div className="bg-[rgba(52,211,153,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(138,255,222,0.15)] transition-all duration-300">
-              {/* icon */}
-              <div className="text-4xl text-emerald-400 mb-4 ">{item.icon}</div>
-              {/* title and description */}
-              <div className="mb-8 ">
-                <div className="mb-2 text-lg "> {item.title}</div>
-                <p className="max-w-[350px] leading-normal ">
-                  {item.description}
-                </p>
-              </div>
-              {/* arrow */}
-              <div className="text-3xl">
-                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-emerald-400 transition-all duration-300" />
-              </div>
+          // <SwiperSlide key={itemIndex}>
+          <div
+            key={itemIndex}
+            className="h-[300px] xl:mr-4 mb-4 bg-[rgba(52,211,153,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(138,255,222,0.15)] transition-all duration-300 w-fit"
+          >
+            {/* icon */}
+            <div className="text-4xl text-emerald-400 mb-4 ">{item.icon}</div>
+            {/* title and description */}
+            <div className="mb-8 ">
+              <div className="mb-2 text-lg "> {item.title}</div>
+              <p className="max-w-[350px] leading-normal ">
+                {item.description}
+              </p>
             </div>
-          </SwiperSlide>
+            {/* arrow */}
+            <div className="text-3xl">
+              <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-emerald-400 transition-all duration-300" />
+            </div>
+          </div>
+          // </SwiperSlide>
         );
       })}
-    </Swiper>
+    </div>
+    // </Swiper>
   );
 };
 
