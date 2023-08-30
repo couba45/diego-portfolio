cff0e958541676c204d62409ca35e866a031f3e4
couba45 <103531034+couba45@users.noreply.github.com>
2023-08-30 18:03:42 -0500

    troubleshooting again

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b279d4ca162a4cfd"; }
+/******/ 		__webpack_require__.h = function() { return "cbb96f5d442cc486"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/about/index.js
+++ pages/about/index.js
@@ -246,36 +246,7 @@ const About = () => {
         </div>
       </div>
       <div className="h-full bg-primary/30 py-36">
-        <div className="h-full md:h-fit xl:h-full  flex items-center overflow-scroll xl:overflow-hidden   no-scrollbar">
-          <div className="container mx-auto">
-            <div className="flex  flex-col xl:flex-row gap-x-8">
-              {/* text */}
-              <div className="text-center flex xl:w-[30vw] flex-col xl:text-left items-center mb-4 xl:mb-0 ">
-                <motion.h2
-                  variants={fadeIn("up", 0.2)}
-                  initial="hidden"
-                  animate="show"
-                  exit="hidden"
-                  className="h2 xl:mt-8  "
-                >
-                  My Services <span className="text-emerald-400">.</span>
-                </motion.h2>
-                <motion.p
-                  variants={fadeIn("up", 0.4)}
-                  initial="hidden"
-                  animate="show"
-                  exit="hidden"
-                  className="mb-4 max-w-[400px] mx-auto lg:mx-0 "
-                >
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Voluptates quo eius tenetur corporis, earum dolor reiciendis
-                  iste beatae? Porro, eum impedit sunt pariatur dolore
-                  accusantium numquam autem illum nisi quis. Sequi iste quasi,
-                  minus neque minima dicta. Harum aliquid quibusdam asperiores
-                  sapiente. Sapiente omnis aut veritatis in earum, tempore quae
-                  excepturi odio
-                </motion.p>
-              </div>
+       
               {/* slider */}
               <motion.div
                 variants={fadeIn("down", 0.6)}
