6c5998ac80f0dae6a7a3b01a84de0225f52cddab
couba45 <103531034+couba45@users.noreply.github.com>
2023-08-29 22:22:42 -0500

    idk why it breaks

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -58,27 +58,23 @@ const ServiceSlider = () => {
     >
       {serviceData.map((item, itemIndex) => {
         return (
-          <div key={itemIndex} className="swiper-wrapper">
-            <SwiperSlide>
-              <div className="bg-[rgba(52,211,153,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(138,255,222,0.15)] transition-all duration-300">
-                {/* icon */}
-                <div className="text-4xl text-emerald-400 mb-4 ">
-                  {item.icon}
-                </div>
-                {/* title and description */}
-                <div className="mb-8 ">
-                  <div className="mb-2 text-lg "> {item.title}</div>
-                  <p className="max-w-[350px] leading-normal ">
-                    {item.description}
-                  </p>
-                </div>
-                {/* arrow */}
-                <div className="text-3xl">
-                  <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-emerald-400 transition-all duration-300" />
-                </div>
+          <SwiperSlide key={itemIndex}>
+            <div className="bg-[rgba(52,211,153,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(138,255,222,0.15)] transition-all duration-300">
+              {/* icon */}
+              <div className="text-4xl text-emerald-400 mb-4 ">{item.icon}</div>
+              {/* title and description */}
+              <div className="mb-8 ">
+                <div className="mb-2 text-lg "> {item.title}</div>
+                <p className="max-w-[350px] leading-normal ">
+                  {item.description}
+                </p>
               </div>
-            </SwiperSlide>
-          </div>
+              {/* arrow */}
+              <div className="text-3xl">
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-emerald-400 transition-all duration-300" />
+              </div>
+            </div>
+          </SwiperSlide>
         );
       })}
     </Swiper>
