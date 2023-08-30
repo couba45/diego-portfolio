688e8c634209967c008556308e406ed66e379cd5
couba45 <103531034+couba45@users.noreply.github.com>
2023-08-29 23:45:17 -0500

    another attempt with the slider

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -4,48 +4,92 @@ const workSlides = {
     {
       images: [
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+          title: "title",
+          path: "/thumb1.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+          title: "title",
+          path: "/thumb2.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb3.jpg',
+          title: "title",
+          path: "/thumb3.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb4.jpg',
+          title: "title",
+          path: "/thumb4.jpg",
         },
       ],
     },
     {
       images: [
         {
-          title: 'title',
-          path: '/thumb4.jpg',
+          title: "title",
+          path: "/thumb4.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+          title: "title",
+          path: "/thumb1.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+          title: "title",
+          path: "/thumb2.jpg",
         },
         {
-          title: 'title',
-          path: '/thumb3.jpg',
+          title: "title",
+          path: "/thumb3.jpg",
         },
       ],
     },
   ],
 };
 
+// import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+import Image from "next/image";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// icons
+
+// import requeried modules
+import { Pagination } from "swiper";
+
+// icons
+import { BsArrowRight } from "react-icons/bs";
+
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="h-[240px]  sm:h-[340px] "
+    >
+      {workSlides.slides.map((slide, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div>
+              {slide.images.map((image, index) => {
+                return (
+                  <div key={index}>
+                    <div>
+                      <Image src={image.path} width={500} height={300} alt="" />
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default WorkSlider;
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,61 @@
+// components
+import WorkSlider from "../../components/WorkSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+
+// framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36">
+      <div className="h-full md:h-fit xl:h-full  flex items-center overflow-scroll xl:overflow-hidden   no-scrollbar">
+        <div className="container mx-auto">
+          <div className="flex  flex-col xl:flex-row gap-x-8">
+            {/* text */}
+            <div className="text-center flex xl:w-[30vw] flex-col xl:text-left items-center mb-4 xl:mb-0 ">
+              <motion.h2
+                variants={fadeIn("up", 0.2)}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className="h2 xl:mt-8  "
+              >
+                My Work <span className="text-emerald-400">.</span>
+              </motion.h2>
+              <motion.p
+                variants={fadeIn("up", 0.4)}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+                className="mb-4 max-w-[400px] mx-auto lg:mx-0 "
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptates quo eius tenetur corporis, earum dolor reiciendis
+                iste beatae? Porro, eum impedit sunt pariatur dolore accusantium
+                numquam autem illum nisi quis. Sequi iste quasi, minus neque
+                minima dicta. Harum aliquid quibusdam asperiores sapiente.
+                Sapiente omnis aut veritatis in earum, tempore quae excepturi
+                odio
+              </motion.p>
+            </div>
+            {/* slider */}
+            <motion.div
+              variants={fadeIn("down", 0.6)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="w-full xl:max-w-[65%] mx-auto"
+            >
+              <WorkSlider />
+            </motion.div>
+          </div>
+        </div>
+        <Bulb />
+      </div>
+    </div>
+  );
 };
 
 export default Work;
