951e6eee46722759c1288fd7d9152cc88c50db56
couba45 <103531034+couba45@users.noreply.github.com>
2023-08-30 21:27:40 -0500

    fix slider finally

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1714677409ba7669"; }
+/******/ 		__webpack_require__.h = function() { return "09db645d2eecde51"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
